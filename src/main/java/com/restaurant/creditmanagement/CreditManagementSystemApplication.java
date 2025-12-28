ee5ed991586a7234cdca4ffa1436066319a8b3df
Prathamesh <prathameshbille@gmail.com>
2025-12-28 13:06:24 +0530

    Intial Spring boot setup with h2 db

---
+++ src/main/java/com/restaurant/creditmanagement/CreditManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.creditmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditManagementSystemApplication.class, args);
+	}
+
+}
