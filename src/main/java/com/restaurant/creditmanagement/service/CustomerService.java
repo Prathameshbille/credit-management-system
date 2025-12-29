a09b35bcf991942cd370633368187c0a52f22fdc
Prathamesh <prathameshbille@gmail.com>
2025-12-29 22:12:24 +0530

    Customer Service added

---
+++ src/main/java/com/restaurant/creditmanagement/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.restaurant.creditmanagement.service;
+
+import com.restaurant.creditmanagement.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer addCustomer(Customer customer);
+
+    List<Customer> getAllCustomers();
+
+    Customer getCustomerById(Long id);
+
+    void deleteCustomer(Long id);
+}
---
+++ src/main/java/com/restaurant/creditmanagement/serviceImpl/CustomerServiceImpl.java
@@ -0,0 +1,44 @@
+package com.restaurant.creditmanagement.serviceImpl;
+
+import com.restaurant.creditmanagement.entity.Customer;
+import com.restaurant.creditmanagement.repository.CustomerRepository;
+import com.restaurant.creditmanagement.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository=customerRepository;
+    }
+
+    @Override
+    public Customer addCustomer(Customer customer) {
+        if(customerRepository.existsByPhoneNumber(customer.getPhoneNumber())){
+            throw new RuntimeException("Customer with this phone number already exists");
+        }
+        customer.setTotalCredit(0.0);
+
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Customer does not exist with id: "+id));
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
